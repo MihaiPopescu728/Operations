79d658e7a693e765892c374bdaaec314a7587d63
Mihai728 <mihaipopescu728@yahoo.com>
2019-10-11 00:01:19 +0300

    Rezolved the expired homework 1

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,11 @@
 package org.fasttrackit;
 
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         LogicalOp.countToOneHundred(120);
         LogicalOp.countToMinusOneHundred(30);
-        LogicalOp.countFromXtoY1(70,80);
-        LogicalOp.countFromXtoY(95,70);
+        LogicalOp.countFromXtoY1(70, 80);
+        LogicalOp.countFromXtoY(95, 70);
         LogicalOp.showEvenNumbers();
         LogicalOp.showOddNumbers();
         System.out.println(LogicalOp.computeSumToOneHundred(30));
