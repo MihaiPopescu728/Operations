cf5e448e3004e7cf5bcec3d05e894e7c230feb4b
Mihai728 <mihaipopescu728@yahoo.com>
2019-10-10 23:57:19 +0300

    Rezolved the expired homework 1

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,18 @@
+package org.fasttrackit;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        LogicalOp.countToOneHundred(120);
+        LogicalOp.countToMinusOneHundred(30);
+        LogicalOp.countFromXtoY1(70,80);
+        LogicalOp.countFromXtoY(95,70);
+        LogicalOp.showEvenNumbers();
+        LogicalOp.showOddNumbers();
+        System.out.println(LogicalOp.computeSumToOneHundred(30));
+        System.out.println(LogicalOp.computeAverage(29));
+        LogicalOp.showAsterisks();
+
+    }
+}
---
+++ src/main/java/org/fasttrackit/LogicalOp.java
@@ -0,0 +1,96 @@
+package org.fasttrackit;
+
+public class LogicalOp {
+    public static void countToOneHundred(int x) {
+        int counter = 100;
+        if (x <= 100) {
+
+            for (int i = x; i <= counter; i++) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = x; i >= 100; i--)
+                System.out.println(i);
+        }
+    }
+
+    public static void countToMinusOneHundred(int x) {
+        int counter = -100;
+        if (x <= -100) {
+            for (int i = x; i <= -100; i++) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = x; i >= -100; i--) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void countFromXtoY1(int x, int y) {
+        for (int i = x; i <= y; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void countFromXtoY(int x, int y) {
+        if (x <= y) {
+            for (int i = x; i <= y; i++) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = y; i <= x; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void showEvenNumbers() {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void showOddNumbers() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+
+
+            }
+        }
+    }
+
+    public static int computeSumToOneHundred(int x) {
+        int sum = 0;
+        for (int i = x; i <= 100; i++) {
+            sum = sum + i;
+
+        }
+        return sum;
+    }
+
+    public static double computeAverage(int x) {
+        int sum = 0;
+        int counter = 0;
+        for (int i = x; i <= 100; i++) {
+            sum = sum + i;
+            counter++;
+        }
+
+        double average = ((double) sum) / counter;
+        return average;
+    }
+
+    public static void showAsterisks() {
+        for (int i = 7; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
